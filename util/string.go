5e03f6461390f6403e4cc9a705558a24c83bd65a
Petri Kallio <kallionpetri@gmail.com>
2021-12-04 09:48:05 +0200

    cobra, binary output (#1)

--- util/string.go
+++ util/string.go
@@ -1,9 +1,9 @@
-package main
+package util
 
 func Reverse(s string) string {
 	rev := ([]byte)(s)
 
-	for i, j := len(s) - 1, 0; i >= 0; i, j = i-1, j+1 {
+	for i, j := len(s)-1, 0; i >= 0; i, j = i-1, j+1 {
 		rev[j] = s[i]
 	}
 
